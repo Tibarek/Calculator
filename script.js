6ba724f1c8a501ee595357390ce516c1497bf234
Tibarek Yuggie <ttibarek@gmail.com>
2025-06-03 09:04:12 +0300

    create add, subtract, multiply, divide and operate functions

---
+++ script.js
@@ -0,0 +1,24 @@
+function add (addend1, addend2){
+    return addend1 + addend2;
+}
+
+function subtract (minuend , subtrahend){
+    return minuend - subtrahend;
+}
+
+function multiply (factor1, factor2){
+    return factor1 * factor2;
+}
+
+function divide (dividend, divisor){
+    return dividend / divisor;
+}
+
+const firstNumber = 0;
+const operator = "+";
+const secondNumber = 0;
+
+function operate(firstNumber, operator, secondNumber){
+
+}
+
